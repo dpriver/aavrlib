1e3c91f49349ed38726df6f6723fda85909aa3ea
Kuronogard <dpinto@ucm.es>
2016-04-28 17:13:33 +0200

    Added commands in travis to install gcc-4.9.0 (commented)

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -1,11 +1,21 @@
 #/bin/sh -f
 
+
+#install a newer avr-gcc version
+#wget ftp://ftp.irisa.fr/pub/mirrors/gcc.gnu.org/gcc/releases/gcc-4.9.0/gcc-4.9.0.tar.bz2
+#bunzip2 -c gcc-4.9.0.tar.bz2 | tar xf -
+#cd gcc-4.9.0
+#mkdir obj-avr
+#cd obj-avr
+#../configure --prefix=$PREFIX --target=avr --enable-languages=c,c++ --disable-nls --disable-libssp --with-dwarf2
+
+#make
+#make install
+
+# install a newer cmake version
 if ( test "`uname -s`" = "Darwin" )
 then
-  #cmake v2.8.12 is installed on the Mac workers now
-  #brew update
-  #brew install cmake
-  echo
+  echo "Already installed a valid version"
 else
   #install a newer cmake since at this time Travis only has version 2.8.7
   echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
