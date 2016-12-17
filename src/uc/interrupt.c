1c305ed66d8ce0b3bf1b3a4f312fe1d587912c4c
Kuronogard <dpinto@ucm.es>
2016-12-17 20:04:59 +0100

    Removed unused variable from interrupt.c

--- src/uc/interrupt.c
+++ src/uc/interrupt.c
@@ -31,8 +31,6 @@
 
 #define N_INTERRUPTS 25
 
-// Declare manually (in assembly) the interrupt vector and the calling routine
-// ISR vectors
 
 __attribute__((section(".def_isr"))) void default_ISR( void ) {
     //asm volatile("rjmp interrupt_vector");
@@ -95,8 +93,6 @@ __attribute__((naked, section(".init9"))) void main_jump (void) {
 }
 
 __attribute__((section(".def_isr"))) void __isr(void) {
-    unsigned int var;
-
     asm volatile(
         "__INT0_isr: \n\t"
         "sei \n\t"
@@ -295,7 +291,6 @@ __attribute__((section(".def_isr"))) void __isr(void) {
           "i"(&isr[USART_UDRE_int]), "i"(&isr[USART_TX_int]), "i"(&isr[ADC_int]), "i"(&isr[EE_READY_int]),
           "i"(&isr[ANALOG_COMP_int]), "i"(&isr[TWI_int]), "i"(&isr[SPM_READY_int])
     );
-
 }
 
 void interrupt_init() {
@@ -313,105 +308,3 @@ void interrupt_attach(interrupt_t interrupt, isr_function new_isr) {
 void interrupt_detach(interrupt_t interrupt) {
     isr[interrupt] = default_ISR;
 }
-
- 
-//ISR(INT0_vect, ISR_BLOCK) {
-    //isr[INT0_int]();
-//}
-
-
-//ISR(INT1_vect, ISR_BLOCK) {
-    //isr[INT1_int]();
-//}
-
-//ISR(PCINT0_vect, ISR_BLOCK) {
-    //isr[PCINT0_int]();
-//}
-
-//ISR(PCINT1_vect, ISR_BLOCK) {
-    //isr[PCINT1_int]();
-//}
-
-//ISR(PCINT2_vect, ISR_BLOCK) {
-    //isr[PCINT2_int]();
-//}
-
-//ISR(WDT_vect, ISR_BLOCK) {
-    //isr[WDT_int]();
-//}
-
-//ISR(TIMER2_COMPA_vect, ISR_BLOCK) {
-    //isr[TIMER2_COMPA_int]();
-//}
-
-//ISR(TIMER2_COMPB_vect, ISR_BLOCK) {
-    //isr[TIMER2_COMPB_int]();
-//}
-
-//ISR(TIMER2_OVF_vect, ISR_BLOCK) {
-    //isr[TIMER2_OVF_int]();
-//}
-
-//ISR(TIMER1_CAPT_vect, ISR_BLOCK) {
-    //isr[TIMER1_CAPT_int]();
-//}
-
-//ISR(TIMER1_COMPA_vect, ISR_BLOCK) {
-    //isr[TIMER1_COMPA_int]();
-//}
-
-//ISR(TIMER1_COMPB_vect, ISR_BLOCK) {
-    //isr[TIMER1_COMPB_int]();
-//}
-
-//ISR(TIMER1_OVF_vect, ISR_BLOCK) {
-    //isr[TIMER1_OVF_int]();
-//}
-
-//ISR(TIMER0_COMPA_vect, ISR_BLOCK) {
-    //isr[TIMER0_COMPA_int]();
-//}
-
-//ISR(TIMER0_COMPB_vect, ISR_BLOCK) {
-    //isr[TIMER0_COMPB_int]();
-//}
-
-//ISR(TIMER0_OVF_vect, ISR_BLOCK) {
-    //isr[TIMER0_OVF_int]();
-//}
-
-//ISR(SPI_STC_vect, ISR_BLOCK) {
-    //isr[SPI_STC_int]();
-//}
-
-//ISR(USART_RX_vect, ISR_BLOCK) {
-    //isr[USART_RX_int]();
-//}
-
-//ISR(USART_UDRE_vect, ISR_BLOCK) {
-    //isr[USART_UDRE_int]();
-//}
-
-//ISR(USART_TX_vect, ISR_BLOCK) {
-    //isr[USART_TX_int]();
-//}
-
-//ISR(ADC_vect, ISR_BLOCK) {
-    //isr[ADC_int]();
-//}
-
-//ISR(EE_READY_vect, ISR_BLOCK) {
-    //isr[EE_READY_int]();
-//}
-
-//ISR(ANALOG_COMP_vect, ISR_BLOCK) {
-    //isr[ANALOG_COMP_int]();
-//}
-
-//ISR(TWI_vect, ISR_BLOCK) {
-    //isr[TWI_int]();
-//}
-
-//ISR(SPM_READY_vect, ISR_BLOCK) {
-    //isr[SPM_READY_int]();
-//}
